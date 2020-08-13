e95a9af0e57cf0a26964e7244e759bb26a89361f
Sharon Fraiman <sharon.fraiman@gm.com>
2020-08-14 00:44:38 +0300

    commit

--- Rust/projects/guessing_game/src/main.rs
+++ Rust/projects/guessing_game/src/main.rs
@@ -1,11 +1,15 @@
 /*
-Guessing game - Chapter 2 of book "The RUST programming language"
+Guessing game - Chapter 2 of book "The RUST programming language" (p.70)
 */
 
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number !");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    println!("The secret number is: {}", secret_number);
 
     println!("Please input your guess.");
 
