48c18233ca1955b6a8b3efcffa9dc34e901dcdf1
Sharon Fraiman <sharon.fraiman@gm.com>
2020-09-06 00:38:38 +0300

    Commit

--- Cpp/State_Machine/main.cpp
+++ Cpp/State_Machine/main.cpp
@@ -1,4 +1,49 @@
 #include <iostream>
+#include <typeinfo>
+
+class Context;
+
+class State 
+{
+protected:
+    Context* m_context;
+
+public: 
+    virtual ~State() { }
+
+    void set_context(Context* context)
+    {
+        m_context = context;
+    }
+
+    virtual void Handle1() = 0;
+    virtual void Handle2() = 0;
+};
+
+class Context 
+{
+private:
+    State* m_state;
+
+public:
+    Context(State* state) : m_state(NULL)
+    {
+        TransitionTo(state);
+    }
+
+    ~Context() 
+    {
+        delete m_state;
+    }
+
+    void TransitionTo(State* state)
+    {
+        
+    }
+};
+
+
+
 
 int main()
 {
